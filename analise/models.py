d7da490476cf29353c210db08ece841b23a9f0db
Igor Almeida <igoralmeeida@hotmail.com>
2021-01-31 21:26:36 -0300

    estrutura

--- analise/models.py
+++ analise/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Estado(models.Model):
+    nome = models.CharField(max_length=50, blank=False, null=False, db_column='Nome')
+    codigoUf = models.IntegerField(blank=False, null=False, db_column='CodigoUf')
+    uf = models.CharField(max_length=2, blank=False, null=False, db_column='Uf')
+    regiao = models.IntegerField(blank=False, null=False, db_column='Regiao')
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=255, blank=False, null=False, db_column='Nome')
+    codigo = models.IntegerField(blank=False, null=False, db_column='Codigo')
+    uf = models.CharField(max_length=2, blank=False, null=False, db_column='Uf') 

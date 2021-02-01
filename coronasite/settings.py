78f9b2219994553ac6b931d9ce81e2ab1a7305e7
Igor Almeida <igoralmeeida@hotmail.com>
2021-01-31 22:51:22 -0300

    config heroku

--- coronasite/settings.py
+++ coronasite/settings.py
@@ -1,21 +1,15 @@
 
 from pathlib import Path
 import os
+from decouple import config
+from dj_database_url import parse as dburl
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$mhjy5w$ujo1^i908=00b5sy5y$go2=#39=mb-x!=d)^ex#(g('
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'coronasiteufape.herokuapp.com']
 
 
 # Application definition
@@ -64,12 +58,8 @@ WSGI_APPLICATION = 'coronasite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 
 # Password validation
